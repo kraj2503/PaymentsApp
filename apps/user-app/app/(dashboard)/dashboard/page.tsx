710164740ac4ae84e724099453bba62406f556d6
kraj2503 <srmkr7800@gmail.com>
2024-10-24 01:02:58 +0530

    added select component

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,10 @@
+import { Card } from "@repo/ui/card";
 
 
 export default function (){
 
     return  <div className="">
         Dashboard Page
+        <Card title="This is card tit" ></Card>
     </div>
 }
